77358f16619da36cc24d2730d706f67b9c5837a1
aivaraskursevicius <aivaras.kursevicius@vodafone.com>
2022-05-09 15:04:57 +0300

    clean up

--- src/features/todo/Todo.js
+++ src/features/todo/Todo.js
@@ -1,11 +1,9 @@
 import React from "react";
 import TaskList from "../../common/TaskList";
-import TransitionGroupExample from "./Card";
 
 const Taxes = () => {
   return (
     <>
-      {/* <TransitionGroupExample /> */}
       <TaskList title="TODO" placeholder="Task" renderAvatar={true} />
     </>
   );
