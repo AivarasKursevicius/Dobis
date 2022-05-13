af8b74ee348f1f960f1104d9cf31d89b4e5661b7
aivaraskursevicius <aivaras.kursevicius@vodafone.com>
2022-05-13 14:37:31 +0300

    removed mock data implemented db data

--- src/features/taxes/TestTable.js
+++ src/features/taxes/TestTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -54,126 +54,6 @@ const Head = [
     owner: "G",
   },
 ];
-const Data = [
-  {
-    id: "123",
-    data: [
-      {
-        id: "111",
-        value: "41",
-      },
-      {
-        id: "222",
-        value: "34",
-      },
-      {
-        id: "333",
-        value: "67",
-      },
-      {
-        id: "444",
-        value: "90",
-      },
-      {
-        id: "555",
-        value: "689",
-      },
-      {
-        id: "666",
-        value: "345",
-      },
-    ],
-  },
-  {
-    id: "456",
-    date: "2022-05-05",
-    data: [
-      {
-        id: "111",
-        value: "42",
-      },
-      {
-        id: "222",
-        value: "34",
-      },
-      {
-        id: "333",
-        value: "67",
-      },
-      {
-        id: "444",
-        value: "90",
-      },
-      {
-        id: "555",
-        value: "689",
-      },
-      {
-        id: "666",
-        value: "345",
-      },
-    ],
-  },
-  {
-    id: "123",
-    data: [
-      {
-        id: "111",
-        value: "43",
-      },
-      {
-        id: "222",
-        value: "34",
-      },
-      {
-        id: "333",
-        value: "67",
-      },
-      {
-        id: "444",
-        value: "90",
-      },
-      {
-        id: "555",
-        value: "689",
-      },
-      {
-        id: "666",
-        value: "345",
-      },
-    ],
-  },
-  {
-    id: "456",
-    date: "2022-05-05",
-    data: [
-      {
-        id: "111",
-        value: "44",
-      },
-      {
-        id: "222",
-        value: "34",
-      },
-      {
-        id: "333",
-        value: "67",
-      },
-      {
-        id: "444",
-        value: "90",
-      },
-      {
-        id: "555",
-        value: "689",
-      },
-      {
-        id: "666",
-        value: "345",
-      },
-    ],
-  },
-];
 
 export default function TestTable(props) {
   const [page, setPage] = useState(0);
@@ -183,6 +63,19 @@ export default function TestTable(props) {
   const [headData, setHeadData] = useState([]);
   const [rowData, setRowData] = useState([]);
   const [newRow, setNewRow] = useState({ data: [] });
+
+  useEffect(() => {
+    const starCountRef = ref(db, `TAXES/DATA`);
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data !== null) {
+        const sortedData = Object.values(data)
+          .slice()
+          .sort((a, b) => b.date - a.date);
+        setRowData(sortedData);
+      }
+    });
+  }, []);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -199,6 +92,7 @@ export default function TestTable(props) {
 
   const handleNewRow = () => {
     const uid = uuid();
+    newRow["date"] = new Date().getTime();
     set(ref(db, `TAXES/DATA/${uid}`), newRow);
     setNewRow({ data: [] });
     props.setIsNewRow(false);
@@ -244,42 +138,6 @@ export default function TestTable(props) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {Data.slice(
-              page * rowsPerPage,
-              page * rowsPerPage + rowsPerPage
-            ).map((row, i) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={i}>
-                  {Head.map((column, index) => {
-                    const isFirst = index === 0;
-                    return row.data.map((a) => {
-                      if (a.id === column.id) {
-                        return (
-                          <React.Fragment key={index}>
-                            {isFirst ? (
-                              <TableCell
-                                onClick={() => handleEditRowToggle()}
-                                align="center"
-                                style={{ width: "80px" }}
-                              >
-                                <IconButton edge="end" aria-label="edit">
-                                  <EditIcon />
-                                </IconButton>
-                              </TableCell>
-                            ) : (
-                              <></>
-                            )}
-                            <TableCell key={column.id} align="left">
-                              {a.value}
-                            </TableCell>
-                          </React.Fragment>
-                        );
-                      }
-                    });
-                  })}
-                </TableRow>
-              );
-            })}
             {props.isNewRow ? (
               <TableRow hover role="checkbox" tabIndex={-1} key="08">
                 <TableCell
@@ -322,13 +180,48 @@ export default function TestTable(props) {
             ) : (
               <></>
             )}
+            {rowData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, i) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={i}>
+                    {Head.map((column, index) => {
+                      const isFirst = index === 0;
+                      return row.data.map((a) => {
+                        if (a.id === column.id) {
+                          return (
+                            <React.Fragment key={index}>
+                              {isFirst ? (
+                                <TableCell
+                                  onClick={() => handleEditRowToggle()}
+                                  align="center"
+                                  style={{ width: "80px" }}
+                                >
+                                  <IconButton edge="end" aria-label="edit">
+                                    <EditIcon />
+                                  </IconButton>
+                                </TableCell>
+                              ) : (
+                                <></>
+                              )}
+                              <TableCell key={column.id} align="left">
+                                {a.value}
+                              </TableCell>
+                            </React.Fragment>
+                          );
+                        }
+                      });
+                    })}
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
         rowsPerPageOptions={[5, 10, 20]}
         component="div"
-        count={Data.length}
+        count={rowData.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
